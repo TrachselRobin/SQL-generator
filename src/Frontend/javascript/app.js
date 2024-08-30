8b0fd10c9a48b781f16ac15097ce0b97a0c77685
trachselr <trachselr@bzz.ch>
2024-08-30 08:49:36 +0200

    ADDED: classes for file creation

--- src/Frontend/javascript/app.js
+++ src/Frontend/javascript/app.js
@@ -3,22 +3,104 @@ class Database {
     constructor(name) {
         this.name = name;
         this.tables = [];
+        this.dropDatabase = false;
+        this.useDatabase = false;
     }
     
-    addTable()
+    addTable(table) {
+        this.tables.push(table);
+    }
+
+    removeTable(table) {
+        this.tables = this.tables.filter(t => t != table);
+    }
+
+    getTables() {
+        return this.tables;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    setName(name) {
+        this.name = name;
+    }
+
+    toString() {
+        let sql = ""
+
+        if(this.dropDatabase) {
+            sql += "DROP DATABASE IF EXISTS `" + this.name + "`;\n\n";
+        }
+
+        if(this.useDatabase) {
+            sql += "USE `" + this.name + "`;\n\n";
+        }
+
+        sql = "CREATE DATABASE `" + this.name + "`;\n\n";
+        
+        for(let i = 0; i < this.tables.length; i++) {
+            sql += this.tables[i].toString() + "\n\n";
+        }
+        
+        return sql;
+    }
 }
 
 class Table {
     constructor(name) {
         this.name = name;
         this.columns = [];
-
-        
     }
     
-    addColumn()
+    addColumn(column) {
+        this.columns.push(column);
+    }
+
+    removeColumn(column) {
+        this.columns = this.columns.filter(c => c != column);
+    }
+
+    getColumns() {
+        return this.columns;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    setName(name) {
+        this.name = name;
+    }
+
+    toString() {
+        let sql = "CREATE TABLE `" + this.name + "` (\n";
+        
+        for(let i = 0; i < this.columns.length; i++) {
+            sql += "    " + this.columns[i].toString();
+            
+            if(i != this.columns.length - 1) {
+                sql += ",";
+            }
+            
+            sql += "\n";
+        }
+        
+        sql += ");";
+        
+        return sql;
+    }
 }
 
+/*
+    example output of toString() method:
+    CREATE TABLE abo (
+        `ID` INTEGER AUTO_INCREMENT, PRIMARY KEY (ID),
+        `name` VARCHAR(20),
+        `price` INTEGER
+    );
+*/
 class Column {
     constructor(name, type, length=null, defaultValue=null, collation=null, attribute=null, nullValue=false, autoIncrement=false, primaryKey=false, comment=null) {
         this.name = name;
@@ -31,6 +113,126 @@ class Column {
         this.autoIncrement = autoIncrement;
         this.primaryKey = primaryKey;
         this.comment = comment;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getType() {
+        return this.type;
+    }
+
+    getLength() {
+        return this.length;
+    }
+
+    getDefault() {
+        return this.default;
+    }
+
+    getCollation() {
+        return this.collation;
+    }
+
+    getAttribute() {
+        return this.attribute;
+    }
+
+    getNull() {
+        return this.null;
+    }
+
+    getAutoIncrement() {
+        return this.autoIncrement;
+    }
+
+    getPrimaryKey() {
+        return this.primaryKey;
+    }
+
+    getComment() {
+        return this.comment;
+    }
+
+    setName(name) {
+        this.name = name;
+    }
+
+    setType(type) {
+        this.type = type;
+    }
+
+    setLength(length) {
+        this.length = length;
+    }
+
+    setDefault(defaultValue) {
+        this.default = defaultValue;
+    }
+
+    setCollation(collation) {
+        this.collation = collation;
+    }
+
+    setAttribute(attribute) {
+        this.attribute = attribute;
+    }
+
+    setNull(nullValue) {
+        this.null = nullValue;
+    }
+
+    setAutoIncrement(autoIncrement) {
+        this.autoIncrement = autoIncrement;
+    }
+
+    setPrimaryKey(primaryKey) {
+        this.primaryKey = primaryKey;
+    }
+
+    setComment(comment) {
+        this.comment = comment;
+    }
+
+    toString() {
+        let sql = "`" + this.name + "` " + this.type;
+
+        if(this.length != null) {
+            sql += "(" + this.length + ")";
+        }
+        
+        if(this.default != null) {
+            sql += " DEFAULT " + this.default;
+        }
+        
+        if(this.collation != null) {
+            sql += " COLLATE " + this.collation;
+        }
+        
+        if(this.attribute != null) {
+            sql += " " + this.attribute;
+        }
+        
+        if(this.null) {
+            sql += " NULL";
+        } else {
+            sql += " NOT NULL";
+        }
+        
+        if(this.autoIncrement) {
+            sql += " AUTO_INCREMENT";
+        }
+        
+        if(this.primaryKey) {
+            sql += " PRIMARY KEY";
+        }
+        
+        if(this.comment != null) {
+            sql += " COMMENT '" + this.comment + "'";
+        }
+        
+        return sql;
     }
 }
 
